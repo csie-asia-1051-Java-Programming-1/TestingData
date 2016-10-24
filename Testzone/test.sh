f6199b879092d2e7aa9f3ac24363307cc21e374b
Yi-Cheng Chen <ChadCYB@users.noreply.github.com>
2016-10-24 15:05:39 +0800

    Update test.sh

--- Testzone/test.sh
+++ Testzone/test.sh
@@ -2,9 +2,3 @@ cd src/ex/
 tail -n +2 "ex01_1050210XX.java" > "ex01_1050210XX.java.tmp" && mv "ex01_1050210XX.java.tmp" "ex01_1050210XX.java"
 javac ex01_1050210XX.java
 java ex01_1050210XX < ../../bot/data1.txt
-javac ex01_1050210XX.java
-java ex01_1050210XX < ../../bot/data2.txt
-javac ex01_1050210XX.java
-java ex01_1050210XX < ../../bot/data1.txt
-javac ex01_1050210XX.java
-java ex01_1050210XX < ../../bot/data1.txt
