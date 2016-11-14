efc9a8f2b217c88031417242cefeeb12a3c22b67
Yi-Cheng Chen <ChadCYB@users.noreply.github.com>
2016-11-14 18:19:07 +0800

    Update test2.sh

--- Testzone/test2.sh
+++ Testzone/test2.sh
@@ -1,7 +1,8 @@
 cd src/ex/
 for MY_FILE in *.java
 do
-	tail -n +2 $MY_FILE > $MY_FILE.tmp && mv $MY_FILE.tmp $MY_FILE
+	echo "tail -n +2 $MY_FILE > $MY_FILE.tmp && mv $MY_FILE.tmp $MY_FILE"
+	
 done
 
 for MY_FILE in *.java
