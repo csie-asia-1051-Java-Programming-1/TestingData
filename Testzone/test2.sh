c288aa7a07a7e987492a37c2d3e796003b8c29b3
Yi-Cheng Chen <Chad.CYB@gmail.com>
2016-11-14 13:23:50 +0800

    sh loop test

---
+++ Testzone/test2.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+cd src/ex/
+for MY_FILE in *.java do
+	tail -n +2 "$MY_FILE" > "$MY_FILE.tmp" && mv "$MY_FILE.tmp" "$MY_FILE"
+done
+
+for MY_FILE in *.java do
+	javac "$MY_FILE"
+done
+
+for ((i = 0; i < 1; i++)) do
+	java ex0"$i"_1050210XX < ../../bot/data"$i".txt
+done
+
