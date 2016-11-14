2d4b7e9141254aa0318f43518908b44f84111726
Yi-Cheng Chen <Chad.CYB@gmail.com>
2016-11-14 15:27:30 +0800

    test2 snytax fix

--- Testzone/test2.sh
+++ Testzone/test2.sh
@@ -1,15 +1,15 @@
-!#/bin/bash
 cd src/ex/
-for MY_FILE in *.java do
-	echo "$MY_FILE"
-	echo "tail -n +2 $MY_FILE > $MY_FILE.tmp && mv $MY_FILE.tmp $MY_FILE"
-	tail -n +2 $MY_FILE > $MY_FILE.tmp && mv $MY_FILE.tmp $MY_FILE
+for MY_FILE in *.java
+do
+	tail -n +2 "$MY_FILE" > "$MY_FILE.tmp" && mv "$MY_FILE.tmp" "$MY_FILE"
 done
 
-for MY_FILE in *.java do
+for MY_FILE in *.java
+do
 	javac "$MY_FILE"
 done
 
-for ((i = 0; i < 1; i++)) do
+for ((i = 0; i < 1; i++))
+do
 	java ex0$i_1050210XX < ../../bot/data$i.txt
 done
