775674030ad609e38486df47b81b09f4b5178ddb
Yi-Cheng Chen <Chad.CYB@gmail.com>
2016-11-14 13:26:41 +0800

    update test2.sh

--- Testzone/test2.sh
+++ Testzone/test2.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 cd src/ex/
 for MY_FILE in *.java do
 	tail -n +2 "$MY_FILE" > "$MY_FILE.tmp" && mv "$MY_FILE.tmp" "$MY_FILE"
@@ -12,4 +10,3 @@ done
 for ((i = 0; i < 1; i++)) do
 	java ex0"$i"_1050210XX < ../../bot/data"$i".txt
 done
-
